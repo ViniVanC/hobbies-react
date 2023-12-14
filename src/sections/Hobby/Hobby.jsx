c2a4d7b89ec2284628b9c35a92a060ec2678824a
ViniVanC <vincentvan0302@gmail.com>
2023-12-14 22:34:39 +0200

    bugfix

--- src/sections/Hobby/Hobby.jsx
+++ src/sections/Hobby/Hobby.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import { Container } from "../../components/Container/Container";
 import { Title } from "../../components/Title/Title";
 import { Description } from "../../components/Description/Description";
+import { useTheme } from "../../context/ThemeContext";
 import "./Hobby.scss";
 
 export const Hobby = ({ id, title, description, icon, reverse = false }) => {
+  const { darkMode } = useTheme();
+
   return (
-    <section id={id} className={`hobby ${reverse ? "reverse" : ""}`}>
+    <section
+      id={id}
+      className={`hobby ${reverse ? "reverse" : ""} ${
+        darkMode ? "" : "light-mode"
+      }`}
+    >
       <Container>
         <div className="hobby__inner">
           <div className="hobby__text-box">
