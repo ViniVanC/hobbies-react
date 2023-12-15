e872d4337d54839647f2587687ee357c5a0d05dd
ViniVanC <vincentvan0302@gmail.com>
2023-12-15 17:14:09 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,44 @@ import { PiCookingPotBold } from "react-icons/pi";
 import { FaGun, FaMusic, FaCode } from "react-icons/fa6";
 import { Element } from "react-scroll";
 
+const data = [
+  {
+    id: 1,
+    name: "karate",
+    title: "title",
+    description: "description",
+    icon: <FaUserNinja />,
+  },
+  {
+    id: 2,
+    name: "sooting",
+    title: "title",
+    description: "description",
+    icon: <FaGun />,
+  },
+  {
+    id: 3,
+    name: "dance",
+    title: "title",
+    description: "description",
+    icon: <FaMusic />,
+  },
+  {
+    id: 4,
+    name: "cooking",
+    title: "title",
+    description: "description",
+    icon: <PiCookingPotBold />,
+  },
+  {
+    id: 5,
+    name: "coding",
+    title: "title",
+    description: "description",
+    icon: <FaCode />,
+  },
+];
+
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [activeSection, setActiveSection] = useState(null);
@@ -32,43 +70,16 @@ function App() {
       <main>
         <Intro handleSetActive={handleSetActive} />
         <div className="hobbies">
-          <Element name={"karate"}>
-            <Hobby
-              title={"title"}
-              description={"description"}
-              icon={<FaUserNinja />}
-            />
-          </Element>
-          <Element name={"sooting"}>
-            <Hobby
-              reverse={true}
-              title={"title"}
-              description={"description"}
-              icon={<FaGun />}
-            />
-          </Element>
-          <Element name={"dance"}>
-            <Hobby
-              title={"title"}
-              description={"description"}
-              icon={<FaMusic />}
-            />
-          </Element>
-          <Element name={"cooking"}>
-            <Hobby
-              reverse={true}
-              title={"title"}
-              description={"description"}
-              icon={<PiCookingPotBold />}
-            />
-          </Element>
-          <Element name={"coding"}>
-            <Hobby
-              title={"title"}
-              description={"description"}
-              icon={<FaCode />}
-            />
-          </Element>
+          {data.map(({ id, name, title, description, icon }) => (
+            <Element key={id} name={name}>
+              <Hobby
+                title={title}
+                description={description}
+                icon={icon}
+                reverse={id % 2 === 0 ? true : false}
+              />
+            </Element>
+          ))}
         </div>
       </main>
       <Footer />
